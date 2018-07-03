670a4b9dd972e5723fefb6b59ad5eda3b73e1df1
Spyros Garyfallos <paloukari@hotmail.com>
2018-07-03 10:25:39 -0700

    updated nugets

--- Templates/nugetBuild.bat
+++ Templates/nugetBuild.bat
@@ -19,8 +19,8 @@ mkdir TypeEdgeModule
 cd TypeEdgeModule
 mkdir content
 cd ..
-mkdir TypeEdgeProxy
-cd TypeEdgeProxy
+mkdir TypeEdgeServiceProxy
+cd TypeEdgeServiceProxy
 mkdir content
 cd ..
 cd ..
@@ -28,13 +28,13 @@ copy TypeEdgeEmulator.nuspec build\TypeEdgeEmulator
 copy TypeEdgeApplication.nuspec build\TypeEdgeApplication
 copy TypeEdgeML.nuspec build\TypeEdgeML
 copy TypeEdgeModule.nuspec build\TypeEdgeModule
-copy TypeEdgeProxy.nuspec build\TypeEdgeProxy
+copy TypeEdgeServiceProxy.nuspec build\TypeEdgeServiceProxy
 
 dotnet build TypeEdgeEmulator
 dotnet build TypeEdgeApplication\TypeEdgeApplication.sln
 dotnet build TypeEdgeML\TypeEdgeML.sln
 dotnet build TypeEdgeModule
-dotnet build TypeEdgeProxy
+dotnet build TypeEdgeServiceProxy
 
 dotnet nuget locals http-cache --clear
 dotnet new --debug:reinit
@@ -42,33 +42,33 @@ dotnet new --debug:reinit
 dotnet new --install TypeEdgeModule
 dotnet new --install TypeEdgeApplication
 dotnet new --install TypeEdgeML
-dotnet new --install TypeEdgeProxy
+dotnet new --install TypeEdgeServiceProxy
 dotnet new --install TypeEdgeEmulator
 
 dotnet clean TypeEdgeModule
 dotnet clean TypeEdgeApplication
 dotnet clean TypeEdgeML
-dotnet clean TypeEdgeProxy
+dotnet clean TypeEdgeServiceProxy
 dotnet clean TypeEdgeEmulator
 
 xcopy TypeEdgeEmulator build\TypeEdgeEmulator\content /s /e /EXCLUDE:list-of-excluded-files.txt
-xcopy TypeEdgeProxy build\TypeEdgeProxy\content /s /e /EXCLUDE:list-of-excluded-files.txt
+xcopy TypeEdgeServiceProxy build\TypeEdgeServiceProxy\content /s /e /EXCLUDE:list-of-excluded-files.txt
 xcopy TypeEdgeModule build\TypeEdgeModule\content /s /e /EXCLUDE:list-of-excluded-files.txt
 xcopy TypeEdgeApplication build\TypeEdgeApplication\content /s /e /EXCLUDE:list-of-excluded-files.txt
 xcopy TypeEdgeML build\TypeEdgeML\content /s /e /EXCLUDE:list-of-excluded-files.txt
 
 nuget.exe pack build\TypeEdgeEmulator -Version %version%
-nuget.exe pack build\TypeEdgeProxy -Version %version%
+nuget.exe pack build\TypeEdgeServiceProxy -Version %version%
 nuget.exe pack build\TypeEdgeModule -Version %version%
 nuget.exe pack build\TypeEdgeApplication -Version %version%
 nuget.exe pack build\TypeEdgeML -Version %version%
 
 move /Y *.nupkg ..\..\TypeEdgeNuGets 
 
-nuget.exe push ..\..\TypeEdgeNuGets\*%version%.nupkg -ApiKey VSTS
+REM nuget.exe push ..\..\TypeEdgeNuGets\*%version%.nupkg -ApiKey VSTS
 
 dotnet nuget locals http-cache --clear
 dotnet new --debug:reinit
 
-dotnet new -i TypeEdge.Application::*
-dotnet new -i TypeEdge.ML::*
+REM dotnet new -i TypeEdge.Application::*
+REM dotnet new -i TypeEdge.ML::*
