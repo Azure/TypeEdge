b36853e06e1264d871532d3168dcdd5fc3a7fb5f
George Hughey <ghughey@umd.edu>
2018-07-06 20:00:35 -0700

    Initial move to a more modular visualizer

--- Modules/TemperatureSensor/Message.cs
+++ Modules/TemperatureSensor/Message.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.Azure.Amqp.Framing;
 
-namespace sendInfo
+namespace Modules
 {
-    class Message
+    public class Message
     {
-        public NewValue NewVal = new NewValue();
-        public double[] FFTResult;
-        public double[] SubtractionGraphResult;
-        public double ReadRate;
+        public String chartName;
+        public String xlabel;
+        public String ylabel;
+        public double[][] points;
+        public String[] headers;
+        public Boolean append;
+        public Boolean fft;
     }
-        public class NewValue
-        {
-            public String[] Headers;
-            public double[] Inputs;
-        }
+    public class VisMessage
+    {
+        public Message[] messages;
+    }
 }
--- Modules/VisualizationWeb/wwwroot/js/visualizer.js
+++ Modules/VisualizationWeb/wwwroot/js/visualizer.js
@@ -5,57 +5,93 @@ connection.start().catch(err => console.error(err.toString()));
 
 var numToDraw = 32; // Note: needs to be a power of 2
 var pause = false;
-var fftResultGlobal;
-var readRateGlobal;
 var frames = 1;
 var frameNum = 0;
-
-google.charts.load('current', { 'packages': ['corechart'] });
-var chart1 = "";
 var chart2 = "";
+var charts = {};
+google.charts.load('current', { 'packages': ['corechart'] });
+
 
 // This pseudo-object is possibly the most important in the file. It contains 
 // header information, all data points, a function to return all points (getLog)
 // and functions to get the top Ten or top 100 points so far. Archer has mentioned
 // storing this in a database instead, which would help reduce space concerns.
-var data = {
-    header: [],
-    points: [],
-    getTop: function (x) {
-        var tempArray = this.points.slice(0, x);
-        tempArray.unshift(this.header);
-        return tempArray;
-    },
-    getTopNums: function (x) {
-        var tempArray2 = [];
-        this.points.slice(0, x).forEach(function(element) {
-            tempArray2.push([element[0], element[1]]);
-        });
-        return tempArray2;
+class Chart {    
+    updatePoints(newPoints) {
+        if (this.append === true) {
+            this.points = newPoints.concat(this.points);
+        }
+        else {
+            this.points = newPoints;
+        }
     }
+    constructor(chartName, xlabel, ylabel, headers, points, append, fft) {
+        
+        this.chartName = chartName;
+        this.xlabel = xlabel;
+        this.ylabel = ylabel;
+        this.headers = headers;
+        this.points = points;
+        this.append = append;
+        this.fft = fft;
+        this.googleChart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+        this.options = {
+            title: chartName, hAxis: { title: xlabel }, vAxis: { title: ylabel }, curveType: 'function', legend: { position: 'bottom' }
+        };
+    }
+}
+
+function getTopNums(chart) {
+    var tempArray2 = [];
+    //chart.points.slice(0, numToDraw).forEach(function (element) {
+    //    tempArray2.push([element[0], element[1]]);
+    //});
+    return tempArray2;
+}
+function getTop(chart) {
+    //chart.points = chart.points.slice(0, numToDraw); // Slice off extra to preserve number in chart
+    var tempArray = chart.points.slice(0, numToDraw);
+    tempArray.unshift(chart.headers);
+    /*console.log(arrays);
+    for (var i = 0; i < chart.headers.length; i++) {
+        arrays[i] = [chart.headers[i]];
+    }
+    console.log(arrays);
+    for (var i = 0; i < tempArray.length; i++) {
+        for (var j = 0; j < arrays.length; j++) {
+            arrays[j].push(tempArray[i][j])
+        }
+    }
+    console.log(arrays);
+    return arrays;*/
+    return tempArray;
 }
 
 // Called when a new message is received.
 // This method accepts an array of strings for header information and an array of strings for inputs.
 connection.on("ReceiveInput", (obj) => {
     const Msg = JSON.parse(obj);
-    readRateGlobal = Msg.ReadRate;
-    fftResultGlobal = Msg.FFTResult;
-    var headers = Msg.NewVal.Headers;
-    var inputs = Msg.NewVal.Inputs;
-    if (data.header === undefined || data.header.length === 0) {
-        data.header = headers;
-    }
+    console.log(Msg);
 
-    var messages = [];
-    inputs.forEach(function (value) { // Add each value to an array, defining a 'point'
-        value = parseFloat(value);
-        messages.push(value);   
-    });
-    data.points.unshift(messages); // Add value to our message queue
-    data.points = data.points.slice(0, numToDraw);
+    // First, check if we have this chart already and it's just an update.
+    
+    var messageArray = Msg.messages;
+    for (var i = 0; i < messageArray.length; i++) {
+        var chart = messageArray[i];
+        if (!charts.hasOwnProperty(chart.chartName)) {
+            // Does not exist, so let's create it.
+            charts[chart.chartName] = new Chart(chart.chartName, chart.xlabel, chart.ylabel, chart.headers, chart.points, chart.append, chart.fft);
+        }
+        else {
+            charts[chart.chartName].updatePoints(chart.points);
+        }
+    }
+    
+    
     if (!pause && frameNum % frames == 0) { // Draw charts if user wants
-        drawChart();
+        for (var i = 0; i < messageArray.length; i++) {
+            drawChart(charts[messageArray[i].chartName]);
+        }
     }
     frameNum += 1
 });
@@ -83,22 +119,14 @@ document.getElementById("displayNum").addEventListener("click",
 
 // This actually draws the chart. Possible parameterization: allow the user to determine
 // How many elements to pull out.
-function drawChart() {
-    var top = data.getTop(numToDraw);
+function drawChart(chart) {
+    var top = getTop(chart);
+    console.log(top);
     var dataTable = google.visualization.arrayToDataTable(top); // This takes care of the first chart
-    var options = {
-        title: 'Timestamp/Value Chart', hAxis: { title: 'Timestamp' }, vAxis: { title: 'Value' }, curveType: 'function', legend: { position: 'bottom' }
-    };
-    if (chart1 != "") {
-        chart1.clearChart();
-    }
-    else {
-        chart1 = new google.visualization.LineChart(document.getElementById('curve_chart'));
-    }
-    console.log(dataTable);
-    chart1.draw(dataTable, options);
+    
+    chart.googleChart.draw(dataTable, chart.options);
 
-    var topNumbers = data.getTopNums(numToDraw);
+    var topNumbers = getTopNums(chart, numToDraw);
 
     // Format array for graph 
     var result = [];
@@ -106,7 +134,6 @@ function drawChart() {
         result.push([(idx*readRateGlobal / fftResultGlobal.length), fftResultGlobal[idx], 0]);
     }
     // Header
-    result.unshift(data.header);
     var dataFFTTable = google.visualization.arrayToDataTable(result);
 
     /* Draw Second Chart */
@@ -118,3 +145,4 @@ function drawChart() {
     }
     chart2.draw(dataFFTTable, options2);
 }
+
