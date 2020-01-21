4aae8929241588ea3e806486cca7d9dfefa72372
Spyros Garyfallos <paloukari@hotmail.com>
2020-01-21 15:47:55 -0800

    updated examples and templates

--- Templates/TypeEdgeML/TypeEdgeML.Shared/Twins/TypeEdgeModule3Twin.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Azure.TypeEdge.Twins;
-
-namespace TypeEdgeML.Shared.Twins
-{
-    public class TypeEdgeModule3Twin : TypeTwin
-    {
-        public int Threshold { get; set; }
-    }
-}
